7d191eac6260f13276e57392b6a500dff54fe906
Vince P <32515747+Peperito@users.noreply.github.com>
2022-08-17 15:09:53 +0200

    "adjusted loader for mobile"

--- src/App.js
+++ src/App.js
@@ -26,9 +26,9 @@ function App() {
           <NavBar />
           <Hero />
 
-        <div className="fixed bottom-1 right-1 group scale-50 group translate-x-12 translate-y-12">
+        <div className="invisible xl:visible fixed bottom-1 right-1 group scale-50 group translate-x-12 translate-y-12">
         <Spline scene="https://prod.spline.design/o2HsbLEwYAeyvbde/scene.splinecode" />
-        <p className="transition-all text-center scale-0 group-hover:scale-100 bg-orange-200 text-slate-900 fixed bottom-10 right-48 p-6 rounded-md shadow-md w-48">
+        <p className="invisible xl:visible transition-all text-center scale-0 group-hover:scale-100 bg-orange-200 text-slate-900 fixed bottom-10 right-48 p-6 rounded-md shadow-md w-48">
           Currently learning more about 3D!
         </p>
         </div>
