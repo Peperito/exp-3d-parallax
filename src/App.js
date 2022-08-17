eff5b69d72ad8d8fde84513eb54f73aa408e82de
Vince P <32515747+Peperito@users.noreply.github.com>
2022-08-17 19:14:10 +0200

    "Added button glow"

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import React, {useState} from 'react';
 import {AnimatePresence, motion} from "framer-motion";
+import Projects from './components/Projects';
 
 function App() {
 
@@ -22,17 +23,21 @@ function App() {
     !loading &&
     <AnimatePresence>
       <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5}}>
-         <main className="bg-hero-pattern bg-cover bg-no-repeat bg-center overflow-visible h-auto w-auto transition-all">
+      
+         <main className="bg-hero-pattern bg-cover bg-no-repeat bg-center overflow-x-hidden h-auto w-auto transition-all">
           <NavBar />
           <Hero />
 
-        <div className="invisible xl:visible fixed bottom-1 right-1 group scale-50 group translate-x-12 translate-y-12">
+        <div className="invisible xl:visible fixed bottom-1 right-1 group scale-50 group translate-x-12 translate-y-12 z-50">
         <Spline scene="https://prod.spline.design/o2HsbLEwYAeyvbde/scene.splinecode" />
         <p className="invisible xl:visible transition-all text-center scale-0 group-hover:scale-100 bg-orange-200 text-slate-900 fixed bottom-10 right-48 p-6 rounded-md shadow-md w-48">
           Currently learning more about 3D!
         </p>
         </div>
         </main>
+        <div className="z-20">
+        <Projects />
+        </div>
       </motion.div>
     </AnimatePresence>
   );
---
+++ src/components/Projects.js
@@ -0,0 +1,19 @@
+import React from "react";
+import shopihomepage from "../img/shopihomepage.png";
+
+export const Projects = () => {
+
+    return (
+        <div className="grid items-start justify-center border">
+        <div className="relative m-4">
+            <div className="absolute -inset-1 bg-gradient-to-r from-purple-400 to-orange-600 rounded-lg blur"></div>
+                <button className="relative px-7 py-6 bg-indigo-800 rounded-lg leading-none flex divide-x divide-gray-600 mx-0 items-center">
+                    <span className="text-orange-300 pr-20">Shopifast </span>
+                    <span className="text-indigo-200 pl-24">Unit</span>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
