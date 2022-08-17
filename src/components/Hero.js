4853e209014b2327b1d29a10a5c457ba450ac4df
Vince P <32515747+Peperito@users.noreply.github.com>
2022-08-17 12:27:30 +0200

    "ADjustment mobile"

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ export const Hero = () => {
 
     return (
         <div className="flex xl:flex-row flex-col justify-start ">
-            <div className="scale-50 xl:scale-100 -translate-x-16 xl:translate-x-4" >
+            <div className="scale-50 xl:scale-100 -translate-x-20 xl:translate-x-4" >
                 <div className="scale-75 xl:scale-100">
                 <Spline scene="https://prod.spline.design/PeoY1AdyiFikuqYF/scene.splinecode" />
                 </div>
@@ -15,13 +15,13 @@ export const Hero = () => {
             <div className="flex flex-row flex-wrap xl:m-24">
                 <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
                 <img src={nodeimg} alt="node" className="h-24 w-24" />
-                <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
-                <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
-                <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
-                <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
-                <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
-                <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
-                <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
+                <img src={PostgresImg} alt="postgres" className="h-20 w-20" />
+                <img src={PostgresImg} alt="postgres" className="h-20 w-20" />
+                <img src={PostgresImg} alt="postgres" className="h-20 w-20" />
+                <img src={PostgresImg} alt="postgres" className="h-20 w-20" />
+                <img src={PostgresImg} alt="postgres" className="h-20 w-20" />
+                <img src={PostgresImg} alt="postgres" className="h-20 w-20" />
+                <img src={PostgresImg} alt="postgres" className="h-20 w-20" />
             </div>
         </div>
     )
