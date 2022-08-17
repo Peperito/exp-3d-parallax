994afd3b0ecbec70adb242fc6510ee64f79d0ebd
Vince P <32515747+Peperito@users.noreply.github.com>
2022-08-17 12:14:48 +0200

    "test again"

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,13 +7,14 @@ export const Hero = () => {
 
     return (
         <div className="flex xl:flex-row flex-col justify-start ">
-            <div className="scale-50 xl:scale-75 -translate-x-20 xl:-translate-x-12" >
+            <div className="scale-50 xl:scale-75 -translate-x-16 xl:-translate-x-12" >
+                <div className="scale-75 xl:scale-100">
                 <Spline scene="https://prod.spline.design/PeoY1AdyiFikuqYF/scene.splinecode" />
+                </div>
             </div>
             <div className="flex flex-row flex-wrap xl:m-24">
                 <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
                 <img src={nodeimg} alt="node" className="h-24 w-24" />
-                <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
                 <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
                 <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
                 <img src={PostgresImg} alt="postgres" className="h-24 w-24" />
