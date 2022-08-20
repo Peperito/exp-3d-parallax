31730661de358538a2931b6f9bb118b184d4da0e
Peperito <32515747+Peperito@users.noreply.github.com>
2022-08-21 00:50:08 +0200

    Update ParallaxBox.js

--- src/components/ParallaxBox.js
+++ src/components/ParallaxBox.js
@@ -6,7 +6,7 @@ export const ParallaxBox = () => {
       return (
         <div className="flex items-center justify-center bg-hero-pattern bg-cover bg-center h-screen bg-no-repeat overflow-hidden">
           <Parallax speed={-10}>
-            <div className="flex items-center justify-center h-auto w-auto bg-salmon-400 rounded-md text-slate-100 shadow-md p-2 m-6 xl:m-12 xl:p-12">
+            <div className="flex items-center justify-center h-auto w-auto bg-salmon-400 rounded-md text-slate-100 shadow-md p-2 my-8 xl:m-12 xl:p-12">
              <p className="text-center text-md xl:text-2xl font-semibold">
                 Beyond Programming
              </p>
@@ -14,21 +14,21 @@ export const ParallaxBox = () => {
           </Parallax>
           <div className="flex flex-col">
             <Parallax speed={-15}>
-              <div className="flex items-center justify-center h-auto w-auto bg-darkBlue-400 rounded-md text-slate-100 shadow-md p-2 m-6 xl:m-12 xl:p-12">
+              <div className="flex items-center justify-center h-auto w-auto bg-darkBlue-400 rounded-md text-slate-100 shadow-md p-2 my-8 xl:m-12 xl:p-12">
                 <p className="text-center text-md xl:text-2xl font-semibold">
                   5 Years Online Marketing Experience
                 </p>
               </div>
             </Parallax>
             <Parallax speed={-15}>
-              <div className="flex items-center justify-center h-auto w-auto bg-specialPurple-400 rounded-md text-slate-100 shadow-md p-2 m-6 xl:m-12 xl:p-12">
+              <div className="flex items-center justify-center h-auto w-auto bg-specialPurple-400 rounded-md text-slate-100 shadow-md p-2 my-8 xl:m-12 xl:p-12">
                 <p className="text-center text-md xl:text-2xl font-semibold">
                   Fluent French and English, German in progress
                 </p>
               </div>
             </Parallax>
             <Parallax speed={-15}>
-              <div className="flex items-center justify-center h-auto w-auto bg-specialIndigo-400 rounded-md text-slate-100 shadow-md p-2 m-6 xl:m-12 xl:p-12">
+              <div className="flex items-center justify-center h-auto w-auto bg-specialIndigo-400 rounded-md text-slate-100 shadow-md p-2 my-8 xl:m-12 xl:p-12">
                 <p className="text-center text-md xl:text-2xl font-semibold">
                   Master in European Business Law
                 </p>
